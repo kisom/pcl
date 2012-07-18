02ae6778c58182a52e7ddc00ac198c293b4cea7d
Kyle Isom <coder@kyleisom.net>
2012-07-17 22:21:21 -0600

    start chapter 11, collections

---
+++ chapter11.lisp
@@ -0,0 +1,8 @@
+;;; code from chapter 11 of Practical Common Lisp, "Collections"
+;;; http://www.gigamonkeys.com/book/collections.html
+
+;; collections: data types collect multiple values into a single
+;; object
+
+;; vectors and lists share enough characteristics that they are
+;; treated as subtypes of the sequence general abstraction
